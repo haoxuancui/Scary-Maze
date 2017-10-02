7da0ea906d3b51471cbc0a01c689305f18d13bb2
tiffanyfederico <tiffanyfederico@192.168.0.18>
2017-10-02 07:58:47 -0600

    next commit

--- Scary Maze2/src/randomCharacter.java
+++ Scary Maze2/src/randomCharacter.java
@@ -7,15 +7,15 @@ public class randomCharacter
 	    private ArrayList<monster> catalogue;
 	    public static void Catalogue()
 	    { 
-	        = new ArrayList<monster>();
-	        randomGenerator = new Random();
+	        //= new ArrayList<monster>();
+	       // randomGenerator = new Random();
 	    }
 
-	    public static void  anyItem()
+	   // public static void  anyItem()
 	    {
 	        int index = randomGenerator.nextInt(catalogue.size());
 	        monster item = catalogue.get(index);
-	        System.out.println("Hello I'm" +  + "our recommendation to you");
-	        return item;
+	        //System.out.println("Hello I'm" +  + "our recommendation to you");
+	        //return item;
 	    }	
 	}
